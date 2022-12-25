c88be6930460e8c58a8ca70dc44d4861f12fb725
Lavesh Satim <lavesh.git@gmail.com>
2022-12-25 11:44:54 +0530

    phase 2 completed

--- test1/src/index.js
+++ test1/src/index.js
@@ -1,19 +1,25 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-
+import ReactDOM from 'react-dom/client'
+import "./style.css"
 
 function Hello() {
+
+    // function on(){
+    //     alert("u clicked button") onClick={on}
+    // }
+
     return (
         <div>
-            <h1>hello world</h1>
+            <h1 style={{ textAlign: "center" }}>hello world</h1>
+            <button className="button" >heyy</button>
         </div>
     );
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
+//ReactDOM.render(<Hello />, document.getElementById('root'))
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+ const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(
-//     <Hello/>
-// )
+ root.render(
+     <Hello/>
+ )
